16421f885f4080b2d400932d4f10abe57be30008
Navya Roshni <navyaroshni06@gmail.com>
2025-07-12 12:31:39 +0530

    Create file1.py

---
+++ file1.py
@@ -0,0 +1,23 @@
+#Task-2
+
+x = input('Enter text to write to the file: ')
+file1 = open('output.txt','w')
+writing_file = file1.write(x + '\n')
+file1.close()
+print('Data successfully written to output.txt.')
+file1.close()
+
+
+y = input('Enter additional text to append: ')
+file1 = open('output.txt','a')
+appending_file = file1.write(y+'\n')
+file1.close()
+print('Data successfully appended.')
+file1.close()
+
+
+print('Final contents of output.txt: ')
+file1 = open('output.txt','r')
+reading_file = file1.read()
+print(reading_file)
+file1.close()
